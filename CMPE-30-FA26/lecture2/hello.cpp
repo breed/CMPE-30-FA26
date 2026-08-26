e99811ea60813223906b80fffa73d86a7f1f962b
ben reed <ben.reed@sjsu.edu>
2026-08-26 08:47:22 -0700

    remove bad paths

--- CMPE-30-FA26/lecture2/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cout << "USAGE: " << argv[0] << " name\n";
-        return 1;
-    }
-    std::cout << "Hello " << argv[1] << "\n";
-}
